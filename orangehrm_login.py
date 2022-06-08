9e860c468aa0923b7727e7dbd7fabef71923848e
lera-4 <46902698+lera-4@users.noreply.github.com>
2022-06-08 21:20:06 +0300

    add test of login

---
+++ orangehrm_login.py
@@ -0,0 +1,14 @@
+
+import time
+from selenium import webdriver # импортируем webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+
+driver = webdriver.Chrome(ChromeDriverManager().install())
+driver.get("https://opensource-demo.orangehrmlive.com/")
+login = driver.find_element_by_xpath("//input[@id='txtUsername']")
+login.send_keys("Admin")
+password = driver.find_element_by_xpath("//input[@id='txtPassword']")
+password.send_keys("admin123")
+login_btn = driver.find_element_by_css_selector(".button")
+login_btn.click()
+driver.quit()
